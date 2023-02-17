7e7829ffc4440ba58c1e0b36c9120085c8a95a69
MarKubil <marius.kub@outlook.com>
2023-02-17 10:15:00 +0000

    Updated starter files, added function logic to ask user questions and create file by the answers

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const fs = require("fs");
+const path = require('path');
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+  {
+    type: 'input',
+    message: 'What is your project title?',
+    name: 'title'
+  },
+  {
+    type: 'input',
+    message: 'README file test',
+    name: 'readme'
+  }
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => err ? console.error(err) : console.log('File Created!'))
+}
+
+// function to initialize program
+function init() {
+  // Asks user questions for README inputs.
+  inquirer.prompt([...questions])
+    // response sends to generateMarkdown then received markdown send to writeToFile.
+    .then((response) => writeToFile(response.readme, generateMarkdown(response)));
+}
+
+// function call to initialize program
+init();
+
+
